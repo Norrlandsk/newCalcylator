01d395ab1d1e87b8811eac062794bd16e85bbfee
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2020-11-23 16:27:56 +0100

    Cleaned up code somewhat, added comments

--- Calcylator/Program.cs
+++ Calcylator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.Collections.Generic;
 
 namespace Calcy
@@ -9,25 +8,31 @@ namespace Calcy
     {
         static void Main(string[] args)
         {
-
+            //Declaring variables
             string operator1 = "";
             string operator2 = "";
             string choice = "";
             string textToEnter = "Calcy";
-            float term1 = 0, term2 = 0, term3 = 0, sum = 0, totSum=0;
+            float term1 = 0, term2 = 0, term3 = 0, sum = 0, totSum = 0;
             bool isRunning = true;
             List<float> sumList = new List<float>();
+
+
+            //Setting title in console
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+            Console.WriteLine("\n>Welcome to Calcy! \n>Please enter operators...");
 
 
-            Console.WriteLine("\n>Welcome to Calcy! \n Please enter operators...");
+            //Initiating program with while loop
             while (isRunning)
             {
+
+                //Prompting user with variables to be used, two operators and three terms
                 Console.Write(">Operator 1:");
                 operator1 = Console.ReadLine();
                 Console.Write(">Operator 2:");
                 operator2 = Console.ReadLine();
-                Console.WriteLine(">Now, enter three terms");
+                Console.WriteLine(">Now, enter three terms...");
                 Console.Write(">Term 1: ");
                 term1 = float.Parse(Console.ReadLine());
                 Console.Write(">Term 2: ");
@@ -35,6 +40,8 @@ namespace Calcy
                 Console.Write(">Term 3: ");
                 term3 = float.Parse(Console.ReadLine());
 
+
+                //Algorithm for deciding which operators to use in which order
                 if (operator1 == "*")
                 {
                     if (operator2 == "*")
@@ -162,61 +169,59 @@ namespace Calcy
                 }
 
 
-
-                Console.WriteLine(">"+term1 + operator1 + term2 + operator2 + term3 + "=" + sum);
+                //Displaying result and saving the result in a List
+                Console.WriteLine(">" + term1 + operator1 + term2 + operator2 + term3 + "=" + sum);
                 sumList.Add(sum);
-                if (sum<100)
+
+
+                //Deciding which message to display to user depending on size of result
+                if (sum < 100)
                 {
                     Console.WriteLine(">The result is less than one hundred");
                 }
-                else if (sum>100)
+                else if (sum > 100)
                 {
                     Console.WriteLine(">The result is more than one hundred");
                 }
-                else 
+                else
                     Console.WriteLine(">Cool, the result is exactly one hundred");
 
+
+
+                //Prompting user about whether to repeat program or quit
                 Console.WriteLine(">Wanna go again? (y/n)");
-                choice=Console.ReadLine();
+                choice = Console.ReadLine();
 
+
+
+                //If user enters "y", go to top of while loop
                 if (choice == "y")
-                { Console.Clear();
+                {
+                    Console.Clear();
                     Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+                    Console.WriteLine(">Please enter operators...");
+
+                }
 
 
-                    Console.WriteLine("\n>Welcome to Calcy! \n Please enter operators...");
-                    continue; }
+                //If user enters "n", iterate through sumList, add the elements and display to user. Bool for while loop is set to false and the program terminates
                 else if (choice == "n")
                 {
                     Console.Clear();
-                    for (int i = 0; i <sumList.Count; i++)
+                    Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (textToEnter.Length / 2)) + "}", textToEnter));
+                    for (int i = 0; i < sumList.Count; i++)
                     {
                         totSum += sumList[i];
-                        
-                        
+
+
                     }
-                    Console.WriteLine("The sum of all results is: " + totSum);
+                    Console.WriteLine(">The sum of all results is: " + totSum + "\n>The application is shutting down...");
                     isRunning = false;
                 }
 
             }
 
-
-
-
-            /* 
-             {
-
-                 Console.WriteLine("Write an expression with three terms and two operators");
-                 var result = new DataTable().Compute(Console.ReadLine(), null);
-                 Console.WriteLine("The result of the expression is: "+result);
-             }*/
-
         }
-
-
-
-
 
     }
 
